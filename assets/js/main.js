b7fdfd4a4eccf8a8e0a5b7ca3b7193575722331e
Isabela Caetano <isabelanyc@gmail.com>
2023-08-21 12:46:47 -0400

    updated nav button

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,11 @@ function toggleNav() {
     var navButton = document.querySelector('.nav-button');
     if (navMenu.style.display === 'block') {
         navMenu.style.display = 'none';
+        navButton.classList.remove('expanded');
         navButton.textContent = 'Menu';
     } else {
         navMenu.style.display = 'block';
+        navButton.classList.add('expanded');
         navButton.textContent = 'Close';
     }
 }
